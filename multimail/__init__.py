720d48628b433738a8be5bed8464df66a44b94ff
Vladimir Caruntu <vladimir@vitaminsoftware.com>
2023-03-02 22:48:20 +0200

    Django 3.2 compatibility

--- multimail/__init__.py
+++ multimail/__init__.py
@@ -2,7 +2,6 @@
 
 .. moduleauthor:: Scott B. Bradley <scott@scott2b.com>, Twitter <@scott2b>
 """
-from django.apps import AppConfig
 
 __version__ = '0.1.5'
 __license__ = 'OSI Approved :: MIT License'
@@ -10,11 +9,3 @@ __license__ = 'OSI Approved :: MIT License'
 __author__ = 'scott2b <Scott B. Bradley>'
 __email__ = 'scott@scott2b.com'
 __url__ = 'http://django-multimail.com'
-default_app_config = 'multimail.MultimailConfig'
-
-
-class MultimailConfig(AppConfig):
-    name = 'multimail'
-
-    def ready(self):
-        from . import signals
